e4026a0e2fd32a28161091c0b315a90c8c80e712
Joey <joeyr699@gmail.com>
2019-11-26 12:03:34 -0500

    began progress on backend

---
+++ backend/model/Player.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Player = new Schema(
+    {
+        name: {
+            type: String
+        },
+        rank: {
+            type: Number
+        },
+        score: {
+            type: Number
+        },
+        time_played: {
+            type: String
+        },
+        game_played: {
+            type: String
+        },
+        status: {
+            type: String
+        } 
+    },
+    {
+        collection: 'players'
+});
+
+module.exports = mongoose.model('Player', Player);
---
+++ backend/routes/player.routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const playerRoute = express.Router();
+
+//Model
+let Player = require('../model/Player');
+
+//Add
+
+//Update
+
+//Delete
+
+//Get All
+
+//Get One
+
+//module.exports = playerRoute;
